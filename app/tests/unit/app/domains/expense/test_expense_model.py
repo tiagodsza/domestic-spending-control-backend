2909babafc538cb0182c165944adc0dce9f541af
Tiago de Souza <tiagodsza@gmail.com>
2020-05-01 12:53:35 -0300

    Created teh package structure

---
+++ app/tests/unit/app/domains/expense/test_expense_model.py
@@ -0,0 +1,25 @@
+# from unittest import TestCase
+#
+#
+# class TestExpenseModel(TestCase):
+#
+#     def test_expense_model_must_be_created(self):
+#         #Arrange
+#         name = 'Leite tirol 2l'
+#         price = 1.60
+#         purchase_date = '2020-05-01 12:05:53.281326'
+#         place_of_purchase = 'Fort Atacadista'
+#
+#         #Action
+#         expense = Expense(
+#             name=name,
+#             price=price,
+#             purchase_date=purchase_date,
+#             place_of_purchase=place_of_purchase,
+#         )
+#
+#         #Assertions
+#         self.assertEqual(expense.name, name)
+#         self.assertEqual(expense.price, price)
+#         self.assertEqual(expense.purchase_date, purchase_date)
+#         self.assertEqual()
