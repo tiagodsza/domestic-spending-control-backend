5f5d3b13d1118040fb501cee16057bdfc842fc28
tiago.souza <tiago.souza@hbsis.com.br>
2020-08-11 20:23:54 -0300

    Created tests to the repository

--- tests/unit/app/database/test_repository.py
+++ tests/unit/app/database/test_repository.py
@@ -48,12 +48,44 @@ class TestRepository(TestCase):
             call.close()
         ])
 
-    # def test_get_must_call_query_and_close(self):
-    #     #Arrange
-    #
-    #     #Action
-    #
-    #     #Asserts
+    def test_get_must_call_query_and_close(self):
+        #Arrange
+        db_mock = Mock()
+        db_mock.query.return_value = 'response'
+        repository = Repository()
+        repository.set_db(db_mock)
+
+        #Action
+        response = repository.get('Model')
+
+        #Asserts
+        self.assertEqual(response, 'response')
+        db_mock_calls = db_mock.mock_calls
+        self.assertEqual(len(db_mock_calls), 2)
+        db_mock.assert_has_calls([
+            call.query('Model'),
+            call.close()
+        ])
+
+    def test_get_by_id_mustcall_query_and_get(self):
+        #Arrange
+        db_mock = Mock()
+        repository = Repository()
+        repository.set_db(db_mock)
+
+        #Action
+        response = repository.get_by_id('Model', 'id')
+
+        #Asserts
+        db_mock_calls = db_mock.mock_calls
+        self.assertEqual(len(db_mock_calls), 3)
+        db_mock.assert_has_calls([
+            call.query('Model'),
+            call.query().get('id'),
+            call.close()
+
+        ])
+        self.assertEqual(response, db_mock.query().get())
 
 
     @patch('app.database.repository.Repository')
